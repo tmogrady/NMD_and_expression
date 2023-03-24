a7a5290cde70644c11b2be16dbac3e4cf94a3e9d
tmogrady <cogrady@tulane.edu>
2023-03-24 11:06:39 -0500

    most inputs at the top now

--- AS_NMD_and_expression.R
+++ AS_NMD_and_expression.R
@@ -5,13 +5,15 @@ library("dplyr")
 library("stringr") # for str_split_fixed
 library("ggplot2")
 
-#Akata CC-115 1 µM ####
-#get expression ####
+#INPUTS ####
 exp <- read.table("../1_NMD_inhibitor_RNA-seq/CC115/Akata_1uM/2_DESeq/Test.deseq2.results.txt", header=TRUE, sep="\t")
-#57285
+nmd_genes <- read.table("../1_NMD_inhibitor_RNA-seq/CC115/Akata_1uM/3_SpliceTools/SETranslateNMD_Akata_CC115_1uM_vs_cntl_test_cntl_SE.MATS.JCEC_FDR_0.0005/6_NMD/gene_lists/SE_NMD.neg_IncDiff.txt",
+                        header = FALSE)
+set_tpm <- 10
 
-#remove EBV genes:
-ann <- read.table("/Applications/Genomics_applications/Genomes_and_transcriptomes/hg38_plus_Akata_inverted.bed.converted.bed")
+#ANALYSIS ####
+#remove EBV genes from DE file:
+ann <- read.table("/reference_files/hg38_plus_Akata_inverted.bed.converted.bed")
 ebv_ann <- ann %>%
   filter(V1 == "chrEBV_Akata_inverted")
 ebv_ann[13:16] <- str_split_fixed(ebv_ann$V4, "_", 4)
@@ -21,30 +23,21 @@ exp <- exp %>%
   filter(!gene %in% ebv_genes)
 #57152
 
-#get NMD gene list ####
-nmd_genes <- read.table("../1_NMD_inhibitor_RNA-seq/CC115/Akata_1uM/3_SpliceTools/SETranslateNMD_Akata_CC115_1uM_vs_cntl_test_cntl_SE.MATS.JCEC_FDR_0.0005/6_NMD/gene_lists/SE_NMD.neg_IncDiff.txt",
-                        header = FALSE)
-
-#put together ####
+#identify NMD-targeted genes in DE file
 exp_nmd_genes <- exp %>%
   mutate(NMD = ifelse(gene %in% nmd_genes$V1, "yes", "no"))
 
-#plot ####
+#first plot ####
 ggplot(exp_nmd_genes, aes(x = log2FC, colour = NMD)) +
   stat_ecdf() +
   xlim(-7.5, 7.5)
 
-#expression filter ####
-set_tpm <- 10
-
+#expression filter: UPDATE WITH COLUMN NAMES ####
+#to improve: make this smarter
 exp_tpm <- exp_nmd_genes %>%
   filter(A0A..TPMs. > set_tpm) %>%
   filter(A0B..TPMs. > set_tpm) %>%
   filter(A0C..TPMs. > set_tpm)
-#7868 for tpm > 10
-
-#reddish: #fbb4ae
-#green: #b2df8a
 
 #pdf("2022-12-09_NMD_log2FC_Akata_CC115_1uM_ks_tpm10_reference.pdf", width = 2.5, height = 3.5)
 ggplot(exp_tpm, aes(x = log2FC, colour = NMD)) +
@@ -88,9 +81,4 @@ no_nmd_fc <- exp_tpm %>%
   pull(log2FC)
 
 ks.test(nmd_fc, no_nmd_fc)
-# Two-sample Kolmogorov-Smirnov test
-# 
-# data:  nmd_fc and no_nmd_fc
-# D = 0.19565, p-value < 2.2e-16
-# alternative hypothesis: two-sided
 
