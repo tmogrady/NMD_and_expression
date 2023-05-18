2a29a5016ecb8f8aa8ec4567700d10f96f45dd7a
tmogrady <cogrady@tulane.edu>
2023-05-18 12:06:23 -0500

    read in data

--- AS_NMD_IncLevelDiff_assessment.R
+++ AS_NMD_IncLevelDiff_assessment.R
@@ -7,10 +7,19 @@ library("stringr") # for str_split_fixed
 library("ggplot2")
 
 #SETUP ####
-ann <- read.table("../3_NMD_and_expression/NMD_and_expression/reference_files/hg38_plus_Akata_inverted.bed.converted.bed")
+ann <- read.table("reference_files/hg38_plus_Akata_inverted.bed.converted.bed")
 ebv_ann <- ann %>%
   filter(V1 == "chrEBV_Akata_inverted")
 ebv_ann[13:16] <- str_split_fixed(ebv_ann$V4, "_", 4)
 ebv_genes <- unique(ebv_ann$V15)
 
 set_tpm <- 10
+
+
+#INPUTS ####
+#get SE
+ds1_se <- read.table("../../1_NMD_inhibitor_RNA-seq/CC115/Mutu_1uM/1_JCEC/Mutu_CC115_1uM_vs_cntl_test_cntl_SE.MATS.JCEC.txt",
+                     header = TRUE)
+#identify NMD-targeting SE
+ds1_nmd_bed_neg <- read.table("../../1_NMD_inhibitor_RNA-seq/CC115/Mutu_1uM/3_SpliceTools/SETranslateNMD_Mutu_CC115_1uM_vs_cntl_test_cntl_SE.MATS.JCEC_FDR_0.0005/6_NMD/SE_NMD_lists.bed/SE_NMD.neg_IncDiff.bed",
+                              header = TRUE)
