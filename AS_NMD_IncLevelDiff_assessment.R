a440480d4b76bcba3e76f9a39c4c180cf51c4da1
tmogrady <cogrady@tulane.edu>
2023-05-18 11:22:13 -0500

    Making a plan

---
+++ AS_NMD_IncLevelDiff_assessment.R
@@ -0,0 +1,3 @@
+# to investigate whether ILDs of NMD-target SE events are different
+# in (putatively) NMD-inhibited conditions vs. splicing alteration conditions
+# based on output from rMATS/SpliceTools
