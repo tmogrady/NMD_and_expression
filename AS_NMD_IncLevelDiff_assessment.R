90f4201a8b4eabb512ab31d2d1846c0f82e3a6a9
tmogrady <cogrady@tulane.edu>
2023-05-18 11:24:06 -0500

    setup for analysis

--- AS_NMD_IncLevelDiff_assessment.R
+++ AS_NMD_IncLevelDiff_assessment.R
@@ -5,3 +5,12 @@
 library("dplyr")
 library("stringr") # for str_split_fixed
 library("ggplot2")
+
+#SETUP ####
+ann <- read.table("../3_NMD_and_expression/NMD_and_expression/reference_files/hg38_plus_Akata_inverted.bed.converted.bed")
+ebv_ann <- ann %>%
+  filter(V1 == "chrEBV_Akata_inverted")
+ebv_ann[13:16] <- str_split_fixed(ebv_ann$V4, "_", 4)
+ebv_genes <- unique(ebv_ann$V15)
+
+set_tpm <- 10
