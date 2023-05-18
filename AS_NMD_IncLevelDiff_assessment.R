a38e7748b88403072ed5d0654a9a7a1f8e750ed3
tmogrady <cogrady@tulane.edu>
2023-05-18 11:23:04 -0500

    necessary packages

--- AS_NMD_IncLevelDiff_assessment.R
+++ AS_NMD_IncLevelDiff_assessment.R
@@ -1,3 +1,7 @@
 # to investigate whether ILDs of NMD-target SE events are different
 # in (putatively) NMD-inhibited conditions vs. splicing alteration conditions
 # based on output from rMATS/SpliceTools
+
+library("dplyr")
+library("stringr") # for str_split_fixed
+library("ggplot2")
